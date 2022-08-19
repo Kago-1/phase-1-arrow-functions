3935049184be72b36826dfa63d9eaf022494dd50
Stephen <stephenkago10@gmail.com>
2022-08-20 00:46:44 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+//arrow functions
+const divide = (num1=2000, num2=100) => num1 / num2;
+divide();
+
+
+const square = (a = 2,b = 2) => a ** b;
+square();
+
+ const add = (num3 = a, num4 = b) => num3 + num4;
+ add();
